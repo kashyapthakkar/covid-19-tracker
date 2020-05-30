77c6f87a2e2be98c82ce9ea44310e57964ac2561
kashyapthakkar <47193812+kashyapthakkar@users.noreply.github.com>
2020-05-29 20:45:44 -0400

    Updates

---
+++ precache-manifest.ef3d386d041f3c875b9e27e652155f8f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2dc2136d680922af02b9b3b79abd5ea",
+    "url": "/covid-19-tracker/index.html"
+  },
+  {
+    "revision": "5827fe803b352f08e0bd",
+    "url": "/covid-19-tracker/static/css/main.70c834df.chunk.css"
+  },
+  {
+    "revision": "f898f26e8490e35f0a6a",
+    "url": "/covid-19-tracker/static/js/2.9577bbe0.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-tracker/static/js/2.9577bbe0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5827fe803b352f08e0bd",
+    "url": "/covid-19-tracker/static/js/main.b8f151d2.chunk.js"
+  },
+  {
+    "revision": "bdd759be6f307e7910dc",
+    "url": "/covid-19-tracker/static/js/runtime-main.3add0288.js"
+  },
+  {
+    "revision": "522dd832996e6111bd54f2ba64196a85",
+    "url": "/covid-19-tracker/static/media/logo.522dd832.png"
+  }
+]);
